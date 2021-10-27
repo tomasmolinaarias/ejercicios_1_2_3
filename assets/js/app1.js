d4a2c31406a63ad9c99042f8beaf44b22242f8b1
tomasmolinaarias <tomasmolinaarias12@gmail.com>
2021-10-27 19:20:26 -0300

    falta arreglar

--- assets/js/app1.js
+++ assets/js/app1.js
@@ -1,3 +1,32 @@
-const nombre = document.getElementById("nombre")
-const asunto = document.getElementById("asunto")
-const mensaje = document.getElementById("mensaje")
+const resultado = document.querySelector('.formulario');
+const formulario = document.querySelector('#formulario');
+const nombre = document.querySelector('#nombre');
+const erro_nombre = document.querySelector('.errorNombre');
+const asunto = document.querySelector('#asunto');
+const erro_asunto = document.querySelector('.errorAsunto');
+const mensaje = document.querySelector('#mensaje');
+const erro_mensaje = document.querySelector('.errorMensaje');
+const boton = document.querySelector('#botton');
+boton.addEventListener('click', () => {
+    event.preventDefault()
+
+    console.log(asunto.value)
+    let entrar = false
+    const sololetras = /I[A-Za-z]+$/g;
+    if (!sololetras.test(nombre.value)) {
+        erro_nombre.innerHTML = 'El nombre es requerido.';
+        entrar = true
+    }
+    console.log(sololetras.test(nombre.value));
+    if (!sololetras.test(asunto.value)) {
+        erro_asunto.innerHTML = 'El nombre es requerido.';
+        entrar = true
+    }
+    console.log(sololetras.test(asunto.value));
+    if (!sololetras.test(mensaje.value)) {
+        erro_mensaje.innerHTML = 'El nombre es requerido.';
+        entrar = true
+    }
+    console.log(sololetras.test(mensaje.value));
+
+});
